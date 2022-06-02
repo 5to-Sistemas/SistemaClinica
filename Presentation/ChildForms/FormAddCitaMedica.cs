b2b19b1cbe26c164fec9ccabdfd90ec6a2a85db3
Ariel <atellezt@est.emi.edu.bo>
2022-06-01 20:51:42 -0400

    filled formaddcitamedica

--- Presentation/ChildForms/FormAddCitaMedica.cs
+++ Presentation/ChildForms/FormAddCitaMedica.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Presentation.ChildForms
 {
-    public partial class FormAddCitaMedica : Form
+    public partial class FormAddCitaMedica : BaseForms.BaseFixedForm
     {
         public FormAddCitaMedica()
         {
